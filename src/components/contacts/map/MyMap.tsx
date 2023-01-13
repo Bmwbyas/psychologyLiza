cb356a0c4113c5ef3f1178164cd2bb9a57d60d60
Askerko Alexandr <askerko50@gmail.com>
2023-01-13 12:24:16 +0300

    fix(mark map)

--- src/components/contacts/map/MyMap.tsx
+++ src/components/contacts/map/MyMap.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import MapGL,{ Popup }  from '@urbica/react-map-gl';
+import MapGL,{ Source, Layer, Image }  from '@urbica/react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-
+import imageURL from './../../../assets/image/mapMark.png'
 
     export  function MyMap(){
         const [newPosition, setNewPosition] = useState<any>({position: "relative"})
-
+         // const imageURL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png';
         const onMouseEnterHandler = () => {
             setNewPosition({position: "revert"})
         }
@@ -16,11 +16,23 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
         const [viewport, setViewport] = useState({
 
-            latitude: 53.928410654360626,
+            latitude: 53.928610654360626,
             longitude: 27.66190368383058,
-            zoom: 16
+            zoom: 17
         });
-
+        const data = {
+            type: 'FeatureCollection',
+            features: [
+                {
+                    type: 'Feature',
+                    properties: {},
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [27.66180368383058, 53.928810654360626]
+                    }
+                }
+            ]
+        };
 
         return (
             <div className='mt-4' style={newPosition} onClick={onMouseEnterHandler}
@@ -32,9 +44,20 @@ import 'mapbox-gl/dist/mapbox-gl.css';
                     onViewportChange={setViewport}
                     {...viewport}
                 >
-                    <Popup  longitude={27.66180368383058} latitude={53.928710654360626} closeButton={false} closeOnClick={false}>
-                        My office
-                    </Popup>
+                    <Source id='point' type='geojson' data={data} />
+                    <Image id='my-image' pixelRatio={1} image={imageURL} />
+                    <Layer
+                        id='image-layer'
+                        type='symbol'
+                        source='point'
+                        layout={{
+                            'icon-image': 'my-image',
+                            'icon-size': 0.25
+                        }}
+                    />
+                    {/*<Popup  longitude={27.66180368383058} latitude={53.928710654360626} closeButton={false} closeOnClick={false}>*/}
+                    {/*    My office*/}
+                    {/*</Popup>*/}
                 </MapGL>
             </div>
         );
